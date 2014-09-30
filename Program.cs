a83d415d50908bdb38de43a639ffe0db5e1757ce
nzau <hervino.nzau@gmail.com>
2014-09-30 10:34:02 +0100

    Ajout d'un tableau d'animaux

--- Program.cs
+++ Program.cs
@@ -1,16 +1,24 @@
 using System;
 
-namespace ZooApp
+namespace zooApp
 {
     class Program
-    {
+    {	
+		// les cages dans le zoo
+        public static Animal[] Zoo = new Animal[3];
+
         static void Main(string[] args)
         {
-			Animal animal = new Animal("Milou","aboie", "chien");
-			
-			animal.Presentation();
-			animal.EmettreCris();
-			
+            Zoo[0] = new Chien("Milou");
+            Zoo[1] = new Chat("miau");
+            Zoo[2] = new Chat("Sisi");
+
+            for (int j = 0; j < 3; j++)
+            {
+                Zoo[j].Presentation();
+                Zoo[j].EmettreCris();
+            }
             Console.ReadLine();
-        }             
-    }
+        }        
+    }            
+}
