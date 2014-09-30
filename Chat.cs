b0bdb91d331a3b222c83df3e670f53c01405e2de
nzau <hervino.nzau@gmail.com>
2014-09-30 10:32:09 +0100

    Ajout de 2 classes : Chat.cs et Chien.cs qui heritent de la classe Animal. Mise à jour de la classe program avec ajout d'un tableau d'animaux et leur presentation dans une boucle for

---
+++ Chat.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace zooApp
+{
+    public class Chat:Animal
+{
+    // méthode constructrice
+    public Chat(String nom_animal)
+        :base(n,"chat","miau, miau") 
+    {
+        
+    }
+}
+}
---
+++ Chien.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace zooApp
+{
+    public class Chien:Animal
+	{
+		public Chien(String nom_animal) 
+        :base(n, "chien", "wou, wou"){
+        
+		}
+	}
+}
